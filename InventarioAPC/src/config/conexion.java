44a73ce3b705bbc795a4a7297d989d04aea08429
Danny Ochoa <daochoap@poligran.edu.co>
2021-02-24 23:39:47 -0500

    Conexión
    Conexión base de datos

---
+++ InventarioAPC/src/config/conexion.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author danny
+ */
+public class conexion {
+    Connection con;
+    public conexion() {
+        try {
+             Class.forName("com.mysql.jdbc.Driver");
+             con = DriverManager.getConnection("jdbc:mysql://localhost:3306/bd_inventarioapc","root","12345");
+        } catch (Exception e) {
+        }
+    }
+    public Connection getConnection() {
+        return con;
+    }
+}
